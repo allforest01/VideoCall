b3da49cc566462f397352506b1ca70236685a379
Tuấn Kiệt <allforest01@gmail.com>
2024-06-09 11:58:43 +0700

    first commit

---
+++ src/main/java/com/allforest01/VideoCall/VideoCallApplication.java
@@ -0,0 +1,13 @@
+package com.allforest01.VideoCall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allforest01/VideoCall/VideoCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allforest01.VideoCall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
